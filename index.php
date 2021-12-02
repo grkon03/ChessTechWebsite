84df3ecfc55de777b75414e6114dafcd70cf059e
grkon <yo.for.user@gmail.com>
2021-12-03 02:51:14 +0900

    headerを少し記述するなどした

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./css/main.css" rel="stylesheet">
+        <title>東工大チェスサークル ChessTech</title>
     </head>
     <body>
+        <header>
+            <div id="header_title">ChessTech</div>
+            <div id="header_menu">
+                <div class="header_menu_item">
+                    <a href="./calender.php">Calender</a>
+                </div>
+            </div>  
+        </header>
     </body>
 </html>
