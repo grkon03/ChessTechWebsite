6110ec23fc9f3f1cf66105c6232f5d29af9d7d62
grkon <yo.for.user@gmail.com>
2021-12-03 11:26:59 +0900

    headerをもう少しすすめた

--- index.php
+++ index.php
@@ -4,16 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="./css/main.css" rel="stylesheet">
+        <script>console.log("prevent Google Chrome css Transition");</script>
         <title>東工大チェスサークル ChessTech</title>
     </head>
     <body>
         <header>
-            <div id="header_title">ChessTech</div>
-            <div id="header_menu">
-                <div class="header_menu_item">
-                    <a href="./calender.php">Calender</a>
+            <div class="floatbox">
+                <div id="header_title">
+                    ChessTech
                 </div>
-            </div>  
+                <div id="header_menu">
+                    <div class="header_menu_item">
+                        <a href="./calender.php">Calender</a>
+                    </div>
+                </div>
+            </div>
         </header>
     </body>
 </html>
