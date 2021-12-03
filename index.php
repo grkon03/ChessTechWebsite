22c5b72e6c5ff7b0fa4796b6f6bc98d3a9f2e42e
grkon <yo.for.user@gmail.com>
2021-12-04 03:58:33 +0900

    topページを作成した

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <link href="./css/setting.css" rel="stylesheet">
         <link href="./css/header.css" rel="stylesheet">
         <link href="./css/footer.css" rel="stylesheet">
+        <link href="./css/main/index.css" rel="stylesheet">
         <script>console.log("prevent Google Chrome css Transition");</script>
         <title>東工大チェスサークル ChessTech</title>
     </head>
@@ -17,7 +18,36 @@
             ?>
         </header>
         <div id="main">
-
+            <section>
+            <h1>東工大チェスサークル ChessTech</h1>
+            <p>
+                2021年6月に設立された、東工大の非公認チェスサークルです。<br />
+                参加したいという方は、<strong>lichessのチーム</strong>からの参加をよろしくお願いします。
+                ご連絡のある方は、<strong>Twitter</strong>のダイレクトメッセージか、<strong>お問い合わせフォーム</strong>からよろしくお願いします。
+            </p>
+            <div class="main_link_set">
+                <a class="main_link_item">
+                    lichess のチーム
+                </a>
+                <a class="main_link_item">
+                    Twitter
+                </a>
+                <a class="main_link_item">
+                    お問い合わせフォーム
+                </a>
+            </div>
+            <p>
+                活動予定, 記録などはこちらです。上のメニューの<strong>Calender</strong>, <strong>Activity</strong>からも閲覧できます。
+            </p>
+            <div class="main_link_set">
+                <a class="main_link_item">
+                    活動予定(カレンダー)
+                </a>
+                <a class="main_link_item">
+                    活動記録
+                </a>
+            </div>
+            </section>
         </div>
         <footer>
             <?php
