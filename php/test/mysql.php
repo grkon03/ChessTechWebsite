b50a0bbc101028404a0af2830340256ecf6cca3a
grkon <yo.for.user@gmail.com>
2021-12-23 20:45:19 +0900

    メンバー新規作成のAPIを作った

--- php/test/mysql.php
+++ php/test/mysql.php
@@ -4,14 +4,47 @@
     $password = 'password';
 
     try {
-        $pdo = new PDO($dsn, $user, $password);
+        $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         echo "接続成功<br />";
+        /*
         $tables = $pdo->query("SHOW TABLES");
         while($re = $tables->fetch(PDO::FETCH_ASSOC)){
             var_dump($re);
         }
+        */
+        /*
+        $new_member = "INSERT INTO Members (id, pass, name, handle_name, grade, authority, position)";
+        $new_member .= " VALUES (:id, :pass, :name, :handle_name, :grade, :authority, :position);";
+
+        $id = "grkon";
+        $pass = "yasu0u327";
+        $name = "安田桜輔";
+        $handle_name = "grkon";
+        $grade = "21B";
+        $authority = "1";
+        $position = "部長";
+
+        $pdo->beginTransaction();
+
+        $stmt = $pdo->prepare($new_member);
+        $stmt->bindValue(":id", $id, PDO::PARAM_STR);
+        $stmt->bindValue(":pass", $pass, PDO::PARAM_STR);
+        $stmt->bindValue(":name", $name, PDO::PARAM_STR);
+        $stmt->bindValue(":handle_name", $handle_name, PDO::PARAM_STR);
+        $stmt->bindValue(":grade", $grade, PDO::PARAM_STR);
+        $stmt->bindValue(":authority", $authority, PDO::PARAM_INT);
+        $stmt->bindValue(":position", $position, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $pdo->commit();
+        */
+
+        echo "正常に完了";
     } catch (PDOException $e) {
         echo "接続失敗: " . $e->getMessage() . "\n";
         exit();
+    } catch (ErrorException $e) {
+        echo "クエリ失敗:" . $e->getMessage() . "\n";
     }
 ?>
--- php/util/mysql.php
+++ php/util/mysql.php
@@ -1,5 +1,16 @@
 <?
     /* MySQL用のAPI */
+
+    /* 型としてのクラス */
+    class Member {
+        public $id;
+        public $pass;
+        public $name;
+        public $handle_name;
+        public $grade;
+        public $authority;
+        public $position;
+    }
     
     $l__dsn = 'mysql:dbname=db_chesstech;host=mysql;port=3306;charset=utf8';
     $l__user = 'user';
@@ -18,5 +29,35 @@
         }
 
         // 関数群
+
+        //メンバーを新規登録
+        public function CreateMember(Member $mem) {
+            $new_member = "INSERT INTO Members (id, pass, name, handle_name, grade, authority, position)";
+            $new_member .= " VALUES (:id, :pass, :name, :handle_name, :grade, :authority, :position);";
+
+            $id = $mem->id;
+            $pass = $mem->pass;
+            $name = $mem->name;
+            $handle_name = $mem->handle_name;
+            $grade = $mem->grade;
+            $authority = $mem->authority;
+            $position = $mem->position;
+
+            $this->pdo->beginTransaction();
+
+            $stmt = $this->pdo->prepare($new_member);
+
+            $stmt->bindValue(":id", $id, PDO::PARAM_STR);
+            $stmt->bindValue(":pass", $pass, PDO::PARAM_STR);
+            $stmt->bindValue(":name", $name, PDO::PARAM_STR);
+            $stmt->bindValue(":handle_name", $handle_name, PDO::PARAM_STR);
+            $stmt->bindValue(":grade", $grade, PDO::PARAM_STR);
+            $stmt->bindValue(":authority", $authority, PDO::PARAM_INT);
+            $stmt->bindValue(":position", $position, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            $this->pdo->commit();
+        }
     }
 ?>
