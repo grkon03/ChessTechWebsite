0ad2cbd6db0629d91087dff4f9efc2f0354b5085
grkon <yo.for.user@gmail.com>
2022-01-16 13:19:13 +0900

    メニューのCSSのリンクを修正

--- php/menu/registSchedules/index.php
+++ php/menu/registSchedules/index.php
@@ -8,7 +8,7 @@
         <link href="./../../css/header.css" rel="stylesheet">
         <link href="./../../css/footer.css" rel="stylesheet">
         <link href="./../../css/menu.css" rel="stylesheet">
-        <link href="./../../css/main/menu/changeProfile/index.css" rel="stylesheet">
+        <link href="./../../css/main/menu/registSchedules/index.css" rel="stylesheet">
         <script>console.log("prevent Google Chrome css Transition");</script>
         <title>予定を登録する | 東工大チェスサークル ChessTech</title>
     </head>
