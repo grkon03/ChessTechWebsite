d5c37b9c45669ad6e9f7ce1f4c8b84df92b8528e
grkon <yo.for.user@gmail.com>
2022-01-25 15:44:33 +0900

    「曜日ごとに決める」部分のプログラムを作成

--- php/menu/registJoinableDays/index.php
+++ php/menu/registJoinableDays/index.php
@@ -36,6 +36,74 @@
             <div id="menu_page_main">
                 <h2>活動可能日を登録する</h2>
                 <div class="menu_page_mini">
+                    <?php
+                        $jd_sun = false;
+                        $jd_mon = false;
+                        $jd_tue = false;
+                        $jd_wed = false;
+                        $jd_thi = false;
+                        $jd_fri = false;
+                        $jd_sat = false;
+
+                        if (isset($_POST["weekform"])) {
+                            $jd_sun = isset($_POST["jd_sun"]);
+                            $jd_mon = isset($_POST["jd_mon"]);
+                            $jd_tue = isset($_POST["jd_tue"]);
+                            $jd_wed = isset($_POST["jd_wed"]);
+                            $jd_thi = isset($_POST["jd_thi"]);
+                            $jd_fri = isset($_POST["jd_fri"]);
+                            $jd_sat = isset($_POST["jd_sat"]);
+                            
+                            $joinable_dayofweek = 0;
+                            if ($jd_sun)
+                                $joinable_dayofweek += 1;
+                            if ($jd_mon)
+                                $joinable_dayofweek += 2;
+                            if ($jd_tue)
+                                $joinable_dayofweek += 4;
+                            if ($jd_wed)
+                                $joinable_dayofweek += 8;
+                            if ($jd_thi)
+                                $joinable_dayofweek += 16;
+                            if ($jd_fri)
+                                $joinable_dayofweek += 32;
+                            if ($jd_sat)
+                                $joinable_dayofweek += 64;
+                            
+                            $new_mem = new Member();
+                            $new_mem->id = $member->id;
+                            $new_mem->joinable_dayofweek = $joinable_dayofweek;
+
+                            $sql_util->UpdateMember($new_mem);
+                        } else {
+                            if ($member->joinable_dayofweek === null) {
+                                $new_mem = new Member();
+                                $new_mem->id = $member->id;
+                                $new_mem->joinable_dayofweek = 0;
+    
+                                $sql_util->UpdateMember($new_mem);
+
+                                $member->joinable_dayofweek = 0;
+                            }
+
+                            $jd_week = $member->joinable_dayofweek;
+
+                            if ($jd_week & 1 != 0)
+                                $jd_sun = true;
+                            if ($jd_week & 2 != 0) 
+                                $jd_mon = true;
+                            if ($jd_week & 4 != 0)
+                                $jd_tue = true;
+                            if ($jd_week & 8 != 0)
+                                $jd_wed = true;
+                            if ($jd_week & 16 != 0)
+                                $jd_thi = true;
+                            if ($jd_week & 32 != 0)
+                                $jd_fri = true;
+                            if ($jd_week & 64 != 0)
+                                $jd_sat = true;
+                        }
+                    ?>
                     <h3>曜日ごとに決める</h3>
                     <p>
                         活動可能な曜日を決定してください。
@@ -45,36 +113,64 @@
                             <table>
                                 <tbody>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_sun"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_sun" value=""
+                                                <?php if ($jd_sun) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>日曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_mon"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_mon" value=""
+                                                <?php if ($jd_mon) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>月曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_tue"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_tue" value=""
+                                                <?php if ($jd_tue) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>火曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_wed"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_wed" value=""
+                                                <?php if ($jd_wed) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>水曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_thi"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_thi" value=""
+                                                <?php if ($jd_thi) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>木曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_fri"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_fri" value=""
+                                                <?php if ($jd_fri) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>金曜日</th>
                                     </tr>
                                     <tr>
-                                        <td><input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_sat"></td>
+                                        <td>
+                                            <input type="checkbox" class="registJD_checkbox" value="参加する" name="jd_sat" value=""
+                                                <?php if ($jd_sat) { echo " checked"; } ?>
+                                            >
+                                        </td>
                                         <th>土曜日</th>
                                     </tr>
                                 </tbody>
                             </table>
-                            <input type="hidden" value="weekform" value="true">
+                            <input type="hidden" name="weekform" value="">
                             <input type="submit" value="登録" id="registJD_weekform_submit">
                         </form>
                     </div>
