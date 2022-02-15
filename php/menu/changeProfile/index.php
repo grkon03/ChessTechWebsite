ef243ad9fc2ade27ea5138db041c955a477b2ec6
grkon <yo.for.user@gmail.com>
2022-02-15 18:31:57 +0900

    changeProfileのPC版のデザインを作成した

--- php/menu/changeProfile/index.php
+++ php/menu/changeProfile/index.php
@@ -22,6 +22,58 @@
         <div id="main">
             <div id="menu_page_main">
                 <h2>プロフィールを変更する</h2>
+                <div id="editPF">
+                    <div id="editPF_formarea">
+                        <form action="./" method="POST">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th>
+                                        <td>
+                                            <input name="ID" value="grkon" class="readonly input_text" readonly>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>名前</th>
+                                        <td>
+                                            <input name="name" value="安田桜輔" class="input_text" required>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>ハンドルネーム</th>
+                                        <td>
+                                            <input name="handle_name" value="grkon" class="input_text" required>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>学年</th>
+                                        <td>
+                                            <input name="grade" value="21B" class="input_text" required>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>権限ランク</th>
+                                        <td>
+                                            <input name="authority" value="1" class="input_text readonly" readonly>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>役職</th>
+                                        <td>
+                                            <input name="position" value="部長, 在籍中" class="input_text readonly" readonly>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th id="submit_th"></th>
+                                        <td>
+                                            <input type="submit" value="変更する" id="editPF_submit">
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                </div>
             </div>
             <div id="menu_page_footer">
                 <a href="../" id="back_to_menu">Menu に戻る</a>
