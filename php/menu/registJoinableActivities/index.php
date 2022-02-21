fac3425321e098b384c21d0ee7971744cb05b39e
grkon <yo.for.user@gmail.com>
2022-02-21 11:58:20 +0900

    registJoinableActivitiesを少し進める

--- php/menu/registJoinableActivities/index.php
+++ php/menu/registJoinableActivities/index.php
@@ -22,6 +22,45 @@
         <div id="main">
             <div id="menu_page_main">
                 <h2>参加/非参加を登録する</h2>
+                <div class="menu_page_mini">
+                    <h3>カレンダーから登録する</h3>
+                    <div id="registJA_byCalender">
+                        <p>
+                            カレンダーのイベントをクリックして、イベントの詳細ページで参加/不参加を入力してください。
+                        </p>
+                        <a href="./../../calender/">
+                            カレンダーへ移動
+                        </a>
+                    </div>
+                </div>
+                <div class="menu_page_mini">
+                    <h3>イベントを選択して登録する</h3>
+                    <div id="registJA_bySelect">
+                        <div class="registJA_bySelect_item">
+                            <h4><a href="./../../calender/">イベントタイトル</a></h4>
+                            <div class="registJA_bySelect_item_form">
+                                <form action="./" method="POST">
+                                    <input type="hidden" name="id" value="02020202">
+                                    <div class="registJA_bySelect_item_form_radio">
+                                        <span class="registJA_bySelect_item_form_radio_item">
+                                            <input type="radio" name="joinable" value="T" checked>参加する
+                                        </span>
+                                        <span class="registJA_bySelect_item_form_radio_item">
+                                            <input type="radio" name="joinable" value="F">参加しない
+                                        </span>
+                                    </div>
+                                    <input class="registJA_bySelect_item_form_submit" type="submit" value="変更">
+                                </form>
+                            </div>
+                        </div>
+                        <?php
+                            if (isset($_POST["id"])) {
+                                $id = $_POST["id"];
+                                $joinable = $_POST["id"] == "T" ? true : false;
+                            }
+                        ?>
+                    </div>
+                </div>
             </div>
             <div id="menu_page_footer">
                 <a href="../" id="back_to_menu">Menu に戻る</a>
