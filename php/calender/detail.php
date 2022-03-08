b2e97f320abddfc4257106b7e641b593aee3fad9
grkon <yo.for.user@gmail.com>
2022-03-09 01:57:37 +0900

    detail.phpのデザインを完成

--- php/calender/detail.php
+++ php/calender/detail.php
@@ -3,12 +3,12 @@
 
     $logined = isset($_SESSION["id"]);
 
+    require_once("../util/mysql.php");
+
+    $sql_util = new MYSQL_UTIL();
+
     if ($logined) {
         $id = $_SESSION["id"];
-
-        require_once("../util/mysql.php");
-
-        $sql_util = new MYSQL_UTIL();
 
         $member = $sql_util->GetMember($id);
     }
@@ -35,37 +35,86 @@
         </header>
         <div id="main">
             <div id="detail">
-                <h1>イベント1の詳細</h1>
-                <table id="detail_table">
-                    <tbody>
-                        <tr>
-                            <th>イベント名</th>
-                            <td>イベント1</td>
-                        </tr>
-                        <tr>
-                            <th>開始時間</th>
-                            <td>2022/06/12 20:00:00</td>
-                        </tr>
-                        <tr>
-                            <th>終了時間</th>
-                            <td>2022/06/12 21:00:00</td>
-                        </tr>
-                        <tr>
-                            <th>詳細説明</th>
-                            <td>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細
-                            詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>参加者</th>
-                            <td>grkon</td>
-                        </tr>
-                        <tr>
-                            <th>不参加者</th>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
+            <?php
+                $NotSeletedId = false;
+                $NotExistId = false;
+                $sch = new Schedule();
+                if (!isset($_GET["id"])) {
+                    $NotSelectedId = true;
+                } else if (($sch = $sql_util->GetSchedule($_GET["id"])) === null) {
+                    $NotExistId = true;
+                }
+
+                if ($NotSelectedId) {
+                    echo <<<EOF
+                <h1>エラー</h1>
+                <p>
+                    イベントが指定されていません。<br /><br />
+                    <a href="./">カレンダーに戻る</a>
+                </p>
+EOF;
+                } else if ($NotExistId) {
+                    echo <<<EOF
+                <h1>エラー</h1>
+                <p>
+                    存在しないイベントIDです。<br /><br />
+                    <a href="./">カレンダーに戻る</a>
+                </p>
+EOF;
+                } else {
+                    $start = $sch->date_start->format("Y/m/d H:i:s");
+                    $end = $sch->date_end->format("Y/m/d H:i:s");
+                    echo <<<EOF
+                    <h1>{$sch->name}の詳細</h1>
+                    <table id="detail_table">
+                        <tbody>
+                            <tr>
+                                <th>イベント名</th>
+                                <td>{$sch->name}</td>
+                            </tr>
+                            <tr>
+                                <th>開始時間</th>
+                                <td>{$start}</td>
+                            </tr>
+                            <tr>
+                                <th>終了時間</th>
+                                <td>{$end}</td>
+                            </tr>
+                            <tr>
+                                <th>詳細説明</th>
+                                <td>
+                                {$sch->detail}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>参加者</th>
+                                <td>{$sch->members_join}</td>
+                            </tr>
+                            <tr>
+                                <th>不参加者</th>
+                                <td>{$sch->members_notjoin}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+EOF;
+                }
+            ?>
+                <div id="detail_decide_joinable">
+                    <p id="detail_joinable_message">
+                        あなたは現在「
+                        <span id="detail_joinable_state_T">参加</span>
+                        <span id="detail_joinable_state_F">不参加</span>
+                        」に登録されています。
+                    </p>
+                    <form action="./detail.php?id=<?php echo $sch->id; ?>" method="POST" id="detail_decide_joinable_form_true">
+                        <input type="hidden" name="joinable" value="T">
+                        <input type="submit" value="参加">
+                    </form>
+                    <form action="./detail.php?id=<?php echo $sch->id; ?>" method="POST" id="detail_decide_joinable_form_false">
+                        <input type="hidden" name="joinable" value="F">
+                        <input type="submit" value="不参加">
+                    </form>
+                </div>
             </div>
         </div>
         <footer>
