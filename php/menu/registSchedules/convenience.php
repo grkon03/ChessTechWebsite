698691510880aaf2eed3132f48baa2c221014b40
grkon <yo.for.user@gmail.com>
2022-03-16 14:22:26 +0900

    view_convenienceのデザイン作成

--- php/menu/registSchedules/convenience.php
+++ php/menu/registSchedules/convenience.php
@@ -21,9 +21,21 @@
         <link href="./../../css/header.css" rel="stylesheet">
         <link href="./../../css/footer.css" rel="stylesheet">
         <link href="./../../css/menu.css" rel="stylesheet">
-        <link href="./../../css/main/menu/registSchedules/who.css" rel="stylesheet">
+        <link href="./../../css/main/menu/registSchedules/convenience.css" rel="stylesheet">
         <script>console.log("prevent Google Chrome css Transition");</script>
-        <title>予定を登録する | 東工大チェスサークル ChessTech</title>
+
+        <?php
+            $title = "";
+            $date = null;
+            $specified_correctly = isset($_GET["date"]) && preg_match('/\A[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}\z/', $_GET["date"]);
+            if ($specified_correctly) {
+                $date = new DateTime($_GET["date"]);
+                $title = $date->format("Y/m/d") . "の各部員の都合";
+            } else {
+                $title =  "日付を指定してください。";
+            }
+        ?>
+        <title><?php echo $title; ?> | 東工大チェスサークル ChessTech</title>
     </head>
     <body>
         <header>
@@ -34,7 +46,35 @@
         </header>
         <div id="main">
             <div id="menu_page_main">
-                <h2></h2>
+                <h2><?php echo $title; ?></h2>
+                <div id="view_member_convenience" class="menu_page_mini">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>参加可能</th>
+                                <td>grkon</td>
+                            </tr>
+                            <tr>
+                                <th>予定未定</th>
+                                <td>taroimo</td>
+                            </tr>
+                            <tr>
+                                <th>参加不可</th>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                        if (!$specified_correctly) {
+                            echo "日付を指定してください。";
+                        } else {
+                            
+                        }
+                    ?>
+                    <a id="back_to_index" href="./">
+                        予定作成に戻る
+                    </a>
+                </div>
             </div>
             <div id="menu_page_footer">
                 <a href="../" id="back_to_menu">Menu に戻る</a>
