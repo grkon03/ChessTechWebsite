d5cac5107f654ef5b7019b4702c2057341a04cee
grkon <yo.for.user@gmail.com>
2022-05-22 15:04:09 +0900

    changeMPFのデザインを作成

---
+++ php/menu/changeMembersProfile/index.php
@@ -0,0 +1,85 @@
+<?php
+    session_start();
+    $id = $_SESSION["id"];
+
+    if ($id == "") {
+        header("Location: ./../login.php");
+    }
+
+    require_once(dirname(__FILE__) . "/../../util/mysql.php");
+    $sql_util = new MYSQL_UTIL();
+    
+    $member = $sql_util->GetMember($id);
+
+    if ($member->authority > 1) {
+        header("Location: ./../index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="東工大チェスサークルChessTechの部内メニューです。ここでは各メンバーのプロフィールを編集できます。">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./../../css/setting.css" rel="stylesheet">
+        <link href="./../../css/header.css" rel="stylesheet">
+        <link href="./../../css/footer.css" rel="stylesheet">
+        <link href="./../../css/menu.css" rel="stylesheet">
+        <link href="./../../css/main/menu/changeMembersProfile/index.css" rel="stylesheet">
+        <script>console.log("prevent Google Chrome css Transition");</script>
+        <title>メンバーのプロフィールを変更する | 東工大チェスサークル ChessTech</title>
+    </head>
+    <body>
+        <header>
+            <?php
+                require_once(dirname(__FILE__) . "/../../template/header_template.php");
+                write_header(2);
+            ?>
+        </header>
+        <div id="main">
+            <div id="menu_page_main">
+                <h2>メンバーのプロフィールを変更する</h2>
+                <div class="menu_page_mini" id="changeMPF">
+                    <h3>メンバーのプロフィールを直接変更する</h3>
+                    <p>
+                        プロフィールを変更するメンバーを指定してください。
+                    </p>
+                    <div id="changeMPF_selectMember">
+                        <a class="changeMPF_selectMember_item" href="./changeMPF.php?id=testID">
+                            <span class="changeMPF_selectMember_item_id">
+                                testID
+                            </span>
+                            <span class="changeMPF_selectMember_item_name">
+                                name
+                            </span>/<span class="changeMPF_selectMember_item_handle_name">
+                                handle_name
+                            </span>
+                        </a>
+                        <a class="changeMPF_selectMember_item">
+                            <span class="changeMPF_selectMember_item_id">
+                                testID
+                            </span>
+                            <span class="changeMPF_selectMember_item_name">
+                                name
+                            </span>/<span class="changeMPF_selectMember_item_handle_name">
+                                handle_name
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="menu_page_mini" id="changeAuthority">
+                    <h3>メンバーの権限ランクを変更する</h3>
+                </div>
+            </div>
+            <div id="menu_page_footer">
+                <a href="../" id="back_to_menu">Menu に戻る</a>
+            </div>
+        </div>
+        <footer>
+            <?php
+                require_once(dirname(__FILE__) . "/../../template/footer_template.php");
+                write_footer(2);
+            ?>
+        </footer>
+    </body>
+</html>
